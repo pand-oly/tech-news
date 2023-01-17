02a1290822a374c0b0da8d7b4c7797f2aae04464
pand-oly <hoa98hoa@gmail.com>
2023-01-17 16:48:00 -0300

    feat(Requirement 2): Scrape_updates function returns trybe blog news link list and typing return fetch function of requirement 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,28 +1,32 @@
-# Requisito 1
-import requests
 from time import sleep
+import requests
+from parsel import Selector
+from typing import List, Union
 
 
-def fetch(url: str):
+# Requisito 1
+def fetch(url: str) -> Union[str, None]:
     """Seu código deve vir aqui"""
     headers = {"user-agent": "Fake user-agent"}
-    
     sleep(1)
-    
+
     try:
         response = requests.get(url=url, timeout=3, headers=headers)
     except requests.ReadTimeout:
         return None
-    
+
     if response.status_code != 200:
         return None
 
     return response.text
 
+
 # Requisito 2
-def scrape_updates(html_content):
+def scrape_updates(html_content: str) -> List:
     """Seu código deve vir aqui"""
-    ...
+    selector = Selector(html_content)
+    list_href = selector.css(".cs-overlay a::attr(href)").getall()
+    return list_href
 
 
 # Requisito 3
