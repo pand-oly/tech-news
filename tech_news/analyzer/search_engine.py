ded7b921648007d11eb3adc33742d3a9efd17eb2
pand-oly <hoa98hoa@gmail.com>
2023-01-18 16:12:11 -0300

    feat(Requirement 7): Function get in db news by date and return list of news tuple or empity list, or date is format invalid thrown Value Error

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,14 @@
 from tech_news.database import search_news
-from typing import List, Tuple
+from typing import List, Tuple, Union, Dict
+from datetime import datetime as dt
+
+
+def generate_tuple(list_news: List[Dict]) -> List[Tuple]:
+    list_tuples = list()
+    for news in list_news:
+        list_tuples.append((news["title"], news["url"]))
+
+    return list_tuples
 
 
 # Requisito 6
@@ -12,16 +21,25 @@ def search_by_title(title: str) -> List[Tuple]:
         }
     })
 
-    list_news = list()
-    for news in response_db:
-        list_news.append((news["title"], news["url"]))
-
+    list_news = generate_tuple(response_db)
     return list_news
 
 
 # Requisito 7
-def search_by_date(date):
+def search_by_date(date_string: str) -> List[Union[Tuple, None]]:
     """Seu código deve vir aqui"""
+    try:
+        format_date = dt.strptime(date_string, "%Y-%m-%d")\
+            .date().strftime("%d/%m/%Y")
+        response_db = search_news({"timestamp": format_date})
+
+        list_news = list()
+        if len(response_db) != 0:
+            list_news = generate_tuple(response_db)
+
+        return list_news
+    except ValueError:
+        raise ValueError('Data inválida')
 
 
 # Requisito 8
