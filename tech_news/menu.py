b388844225cdfa8c085e69c26eba79e314cf0af8
pand-oly <hoa98hoa@gmail.com>
2023-01-24 14:03:07 -0300

    feat(Requeriment 13):

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,16 +1,19 @@
-from tech_news.analyzer import ratings, search_engine
+from tech_news.analyzer.search_engine import \
+    search_by_category, search_by_title, search_by_tag, search_by_date
+from tech_news.analyzer.ratings import top_5_categories, top_5_news
 from tech_news.scraper import get_tech_news
+import sys
 
 string_menu = \
-        f"Selecione uma das opções a seguir:\n"\
-        f" 0 - Popular o banco com notícias;\n"\
-        f" 1 - Buscar notícias por título;\n"\
-        f" 2 - Buscar notícias por data;\n"\
-        f" 3 - Buscar notícias por tag;\n"\
-        f" 4 - Buscar notícias por categoria;\n"\
-        f" 5 - Listar top 5 notícias;\n"\
-        f" 6 - Listar top 5 categorias;\n"\
-        f" 7 - Sair.\n"
+        "Selecione uma das opções a seguir:\n"\
+        " 0 - Popular o banco com notícias;\n"\
+        " 1 - Buscar notícias por título;\n"\
+        " 2 - Buscar notícias por data;\n"\
+        " 3 - Buscar notícias por tag;\n"\
+        " 4 - Buscar notícias por categoria;\n"\
+        " 5 - Listar top 5 notícias;\n"\
+        " 6 - Listar top 5 categorias;\n"\
+        " 7 - Sair.\n"
 
 string_input = {
     "0": "Digite quantas notícias serão buscadas: ",
@@ -22,18 +25,18 @@ string_input = {
 
 functions_list = {
     "0": lambda parm: get_tech_news(int(parm)),
-    "1": search_engine.search_by_title,
-    "2": search_engine.search_by_date,
-    "3": search_engine.search_by_tag,
-    "4": search_engine.search_by_category,
-    "5": ratings.top_5_news,
-    "6": ratings.top_5_categories,
+    "1": lambda parm: search_by_title(parm),
+    "2": lambda parm: search_by_date(parm),
+    "3": lambda parm: search_by_tag(parm),
+    "4": lambda parm: search_by_category(parm),
+    "5": lambda: top_5_news(),
+    "6": lambda: top_5_categories()
 }
 
 
 # Requisito 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""    
+    """Seu código deve vir aqui"""
     try:
         input_menu = input(string_menu).strip()
 
@@ -50,9 +53,5 @@ def analyzer_menu():
 
         print(result)
     except KeyError:
-        print('Opção inválida\n')
+        print('Opção inválida', file=sys.stderr)
         return 2
-
-
-if __name__ == "__main__":
-    analyzer_menu()
